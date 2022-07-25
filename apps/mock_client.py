6698ba4df1c2f47a8baa4b11b5cd708eec2841a5
Thomas Bosman <tbosman@gmail.com>
2022-07-25 14:48:15 +0200

    add port arg

--- apps/mock_client.py
+++ apps/mock_client.py
@@ -10,16 +10,17 @@ from models.batch_auction_model import SettledBatchAuction
 
 
 async def sim_client():
-    async with websockets.connect("ws://localhost:8001/", ping_interval=10, ping_timeout=20) as websocket:
+    # async with websockets.connect("ws://localhost:8001/", ping_interval=10, ping_timeout=20) as websocket:
+    async with websockets.connect("wss://wintermute.plmsolver.link/", ping_interval=10, ping_timeout=20) as websocket:
         while True:
-            sol = open('/Users/tbosman/git_tree/1inch-solver/service/sol_4202.json', 'r').read()
-            msg = SimulationRequest(SettledBatchAuction.from_json(sol), True).to_json()
+                sol = open('/Users/tbosman/git_tree/1inch-solver/service/sol_4202.json', 'r').read()
+                msg = SimulationRequest(SettledBatchAuction.from_json(sol), True).to_json()
 
-            await websocket.send(msg)
-            response = await websocket.recv()
-            print('Poing!')
-            print(response)
-            time.sleep(10)
+                await websocket.send(msg)
+                response = await websocket.recv()
+                print('Poing!')
+                print(response)
+                time.sleep(10)
 
 if __name__ == "__main__":
     asyncio.run(sim_client())
--- apps/simulator_service.py
+++ apps/simulator_service.py
@@ -1,4 +1,5 @@
 import asyncio
+import argparse
 
 from settlement.datastructures import SimulationResult, SimulationRequest
 from util.defaultlogging import *
@@ -18,7 +19,9 @@ executor = ThreadPoolExecutor(max_workers=30)
 all_tasks = []
 
 
-async def simulate_and_respond(simulator, batch_json, queue, estimate_block=False):
+async def simulate_and_respond(
+    simulator, batch_json, queue, estimate_block=False
+):
     loop = asyncio.get_running_loop()
     try:
         result = await loop.run_in_executor(
@@ -26,16 +29,15 @@ async def simulate_and_respond(simulator, batch_json, queue, estimate_block=Fals
         )
 
         logger.info(f'Simulation passed: {result["passed"]} \n {result}')
-        if result['passed']:
+        if result["passed"]:
             logger.info(f'Gas estimate: {result["gas"]}')
         msg = SimulationResult(batch_json, **result)
-        logger.info(f'Returning message {msg}')
+        logger.info(f"Returning message {msg}")
 
         await queue.put(msg)
 
     except:
-        logger.exception('Bla')
-
+        logger.exception("Bla")
 
 
 async def rcv_msg(
@@ -49,7 +51,11 @@ async def rcv_msg(
             solution = req.solution.to_json()
 
             logger.debug(jmsg)
-            task = asyncio.create_task(simulate_and_respond(simulator, solution, queue, req.estimate_block))
+            task = asyncio.create_task(
+                simulate_and_respond(
+                    simulator, solution, queue, req.estimate_block
+                )
+            )
             all_tasks.append(task)
 
         except Exception as e:
@@ -61,23 +67,25 @@ async def send_msg(websocket, queue):
         while True:
             msg = await queue.get()
             try:
-                logger.info(f'Sending msg: {msg.to_json()}')
+                logger.info(f"Sending msg: {msg.to_json()}")
             except:
-                logging.exception('Problem')
+                logging.exception("Problem")
             await websocket.send(msg.to_json())
     except websockets.ConnectionClosedOK:
         return
 
 
-async def handler(websocket: WebSocketServerProtocol, path:str):
+async def handler(websocket: WebSocketServerProtocol, path: str):
 
     sockets.add(websocket)
     queue = asyncio.Queue()
     simulator = SettlementSimulator()
 
     try:
-        tasks = [asyncio.create_task(rcv_msg(websocket, simulator, queue)),
-                                     asyncio.create_task(send_msg(websocket, queue))]
+        tasks = [
+            asyncio.create_task(rcv_msg(websocket, simulator, queue)),
+            asyncio.create_task(send_msg(websocket, queue)),
+        ]
         await asyncio.wait(tasks)
 
     except websockets.ConnectionClosedOK:
@@ -86,12 +94,19 @@ async def handler(websocket: WebSocketServerProtocol, path:str):
         sockets.remove(websocket)
 
 
-
-async def main():
-    async with websockets.server.serve(handler, "", 8001, ping_interval=10, ping_timeout=20):
+async def main(port=8001):
+    async with websockets.server.serve(
+        handler, "", port, ping_interval=10, ping_timeout=20
+    ):
         await asyncio.Future()  # run forever
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser("simple_example")
+    parser.add_argument(
+        "--port", help="Port to run on.", type=int, default=8001
+    )
+    args = parser.parse_args()
 
-    asyncio.run(main())
+
+    asyncio.run(main(args.port))
