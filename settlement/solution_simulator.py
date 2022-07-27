52224c8c5f412bb9d3d621b294bedf3017dfa155
Thomas Bosman <tbosman@gmail.com>
2022-07-27 11:40:58 +0200

    estimated block -1

--- settlement/solution_simulator.py
+++ settlement/solution_simulator.py
@@ -238,7 +238,7 @@ class SettlementSimulator:
         try:
             settle_func, tgt_block, call_data = self.get_settle_func_and_block(sol_json, estimate_block)
             result['call_data'] = call_data
-            sim_block = tgt_block if estimate_block else self.block_number
+            sim_block = tgt_block if estimate_block-1 else self.block_number
             result['simulated_block_number'] = sim_block
             gas = settle_func.estimateGas({'gasPrice': self.gas_price,
                                            'from': '0x149d0f9282333681Ee41D30589824b2798E9fb47',
